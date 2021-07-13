0b0d25f790b342d3e5144b033d7078918083bb53
Alekseev0 <rain123567@gmail.com>
2021-07-13 19:06:28 +0300

    Settings update

--- ProzorroPlatform/settings_constants.py
+++ ProzorroPlatform/settings_constants.py
@@ -1,13 +1,13 @@
 #Your settings
 
-EMAIL_PASSWORD = 'Django123'
-EMAIL_USER = 'courseprojectdjango@gmail.com'
+EMAIL_PASSWORD = ''
+EMAIL_USER = ''
 
 
 
-DATABASE_NAME = 'postgres'
-DATABASE_USER = 'postgres'
-DATABASE_PASSWORD = 'Shadow0'
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
 DATABASE_HOST = ''
 DATABASE_PORT = ''
 
